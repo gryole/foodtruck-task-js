ce2ff452680f57c67a0ffe107663581110b18acb
ogrytsenko <okulele.gry@gmail.com>
2024-06-30 10:49:59 +0200

    Add filters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,79 @@
 import React from 'react';
-import './App.css';
-import {Button} from "antd";
+import {Col, Input, Layout, Row, Select, theme} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import NumericInput from "./components/NumericInput";
 
-function App() {
+const {Header, Content, Footer} = Layout;
+
+const style: React.CSSProperties = {padding: '8px 0'};
+
+const App: React.FC = () => {
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken();
+
     return (
-        <div className="App">
-            <Button type="primary">Button</Button>
-        </div>
+        <Layout>
+            <Header style={{display: 'flex', alignItems: 'center'}}>
+                <div className="demo-logo"/>
+            </Header>
+            <Content style={{padding: '0 48px'}}>
+                <div style={{
+                    marginTop: 48,
+                    background: colorBgContainer,
+                    minHeight: 280,
+                    padding: 24,
+                    borderRadius: borderRadiusLG,
+                }}>
+                    <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
+                        <Col className="gutter-row" xs={24} xl={6}>
+                            <div style={style}>
+                                <h5>Food truck name</h5>
+                                <Input size="large" prefix={<UserOutlined/>}/>
+                            </div>
+                        </Col>
+                        <Col className="gutter-row" xs={24} xl={6}>
+                            <div style={style}>
+                                <h5>Food</h5>
+                                <Select
+                                    mode="tags"
+                                    size="large"
+                                    placeholder="Please select"
+                                    defaultValue={['a10', 'c12']}
+                                    style={{width: '100%'}}
+                                />
+                            </div>
+                        </Col>
+                        <Col className="gutter-row" xs={24} xl={6}>
+                            <div style={style}>
+                                <h5>Your location</h5>
+                                <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
+                                    <Col span={12}>
+                                        <NumericInput value="12" onChange={() => {
+                                        }} style={{width: '100%'}} placeholder="Latitude"></NumericInput>
+                                    </Col>
+                                    <Col span={12}>
+                                        <NumericInput value="12" onChange={() => {
+                                        }} style={{}} placeholder="Longitude"></NumericInput>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Col>
+                        <Col className="gutter-row" xs={24} xl={6}>
+                            <div style={style}>
+                                <h5>Search radius, km</h5>
+                                <NumericInput value="5" onChange={() => {
+                                }} style={{}} placeholder="Search radius"></NumericInput>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Content>
+            <Footer style={{textAlign: 'center'}}>
+                Food Truck Inc ©{new Date().getFullYear()}
+            </Footer>
+        </Layout>
     );
-}
+};
 
 export default App;
---
+++ frontend/src/components/NumericInput.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Input} from 'antd';
+
+interface NumericInputProps {
+    style: React.CSSProperties;
+    value: string;
+    onChange: (value: string) => void;
+    placeholder: string
+}
+
+const NumericInput = (props: NumericInputProps) => {
+    const {value, onChange, placeholder} = props;
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {value: inputValue} = e.target;
+        const reg = /^-?\d*(\.\d*)?$/;
+        if (reg.test(inputValue) || inputValue === '' || inputValue === '-') {
+            onChange(inputValue);
+        }
+    };
+
+    // '.' at the end or only '-' in the input box.
+    const handleBlur = () => {
+        let valueTemp = value;
+        if (value.charAt(value.length - 1) === '.' || value === '-') {
+            valueTemp = value.slice(0, -1);
+        }
+        onChange(valueTemp.replace(/0*(\d+)/, '$1'));
+    };
+
+    return (
+
+        <Input
+            {...props}
+            size="large"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder={placeholder}
+            maxLength={16}
+        />
+    );
+};
+
+
+export default NumericInput;
